ed238e8aaaf38a17e28feb86a638a56d55f01bb7
_munemu_ <mdremon1234@outlook.com>
2024-09-17 10:00:27 +0600

    added a greeting function

--- hello.java
+++ hello.java
@@ -1,5 +1,10 @@
 public class Hello{
     public static void main(string[] args){
-        system.out.println("VS Code is awesome")
+        system.out.println("VS Code is awesome.... ");
+        greeting("Munemu")
     }
+
+    public static void greeting(string name){
+        system.out.println("Welcome" + name);
+    } 
 }
